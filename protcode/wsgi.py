fa96f150c6026514e229c919a375844f2fee0e7a
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-03 18:49:27 -0400

    primer comite

--- protcode/wsgi.py
+++ protcode/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 #import heroku
+from whitenoise.django import DjangoWhiteNoise
 #from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protcode.settings")
@@ -19,5 +20,4 @@ application = get_wsgi_application()
 #linea heroku
 #application = Cling(get_wsgi_application())
 
-
-
+application = DjangoWhiteNoise(application)
