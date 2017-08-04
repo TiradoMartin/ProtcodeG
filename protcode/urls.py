7c8635db4d2e174a3b503f608adb365868f14ca3
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-03 20:01:08 -0400

    primer comite

--- protcode/urls.py
+++ protcode/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+     url(r'', include('temple.index.html')),
 ]
 admin.site.site_header = ' Protcode Admin'
